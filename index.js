ccb88f21ed4fd882460f180481a31ac2232bcc65
Ankit Kumar Singh <ankitkumarsingh221b072@gmail.com>
2025-08-06 06:05:14 +0530

    init

---
+++ index.js
@@ -0,0 +1,8 @@
+import express from "express"
+const app = express();
+app.get("/",(req,res)=>{
+    res.send("Hola Amigo")
+})
+app.listen(3000,()=>{
+    console.log("Server started on port 3000");
+});
